c9e142b2bd607c4c1e4b90ac98fa4ecaf3209200
Priya-Yanamandra <arunapriyay@gmail.com>
2026-03-11 14:41:57 +0530

    change app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 X = np.array([1,2,3,4,5]).reshape(-1,1)
-y = np.array([3,5,7,9,11])
+y = np.array([2,4,6,8,10])
 
 def train_model():
     model = LinearRegression()
@@ -16,4 +16,4 @@ def predict_value(value):
 
 if __name__ == "__main__":
     result = predict_value(6)
-    print("Predicted salary for 6 years experience:", result)
+    print("Predicted value:", result)
