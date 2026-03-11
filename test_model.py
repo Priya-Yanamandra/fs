aed4f92eb4514e6fd502a14ca9f4b9e46e7acd64
Priya-Yanamandra <arunapriyay@gmail.com>
2026-03-11 14:20:23 +0530

    test commit 2

--- test_model.py
+++ test_model.py
@@ -5,15 +5,15 @@ X = np.array([[1], [2],[3],[4]])
 y = np.array([2,4,6,8])
 
 def test_coefficient():
-    model = LinerRegression()
+    model = LinearRegression()
     model.fit(X,y)
     assert round(model.coef_[0], 2) == 2.0
 def test_intercept():
-    model = LinerRegression()
+    model = LinearRegression()
     model.fit(X,y)
     assert round(model.intercept_,2) == 0.0
 def test_prediction():
-    model = LinerRegression()
+    model = LinearRegression()
     model.fit(X,y)
     pred = model.predict([[5]])
     assert round(pred[0], 2) == 10.0
